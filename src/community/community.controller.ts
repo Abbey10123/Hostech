2b1139aa6dc1ddd366981a839d328cf8424c3c1a
Temitope <adeyemitemitope2018@gmail.com>
2023-02-12 16:46:37 +0100

    addition of Login API

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -2,16 +2,24 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CommunityService } from './community.service';
 import { CreateCommunityDto } from './dto/create-community.dto';
 import { UpdateCommunityDto } from './dto/update-community.dto';
+import { User } from './interface/user.interface';
+
 
 @Controller('community')
 export class CommunityController {
   constructor(private readonly communityService: CommunityService) {}
 
   @Post()
-  create(@Body() createCommunityDto: CreateCommunityDto) {
-    return this.communityService.create(createCommunityDto);
+  create(@Body() user: User) {
+    return this.communityService.createUser(user);
   }
 
+  @Post('login')
+  login(@Body() loginInfo:User){ // and this
+    return this.communityService.login(loginInfo);
+  }
+
+/*
   @Get()
   findAll() {
     return this.communityService.findAll();
@@ -30,5 +38,5 @@ export class CommunityController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.communityService.remove(+id);
-  }
+  } */
 }
--- src/community/entities/community.entity.ts
+++ src/community/entities/community.entity.ts
@@ -1,1 +1,50 @@
-export class Community {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Gender, User, UserType } from "../interface/user.interface";
+
+
+@Entity({
+    name:' community',
+})
+export class CommunityEntity implements User {
+
+    @PrimaryGeneratedColumn()
+     id:number;
+     
+     @Column({name:'full_name' })
+     fullName: string;
+
+     @Column({name:'phone_number'})
+     phoneNumber: string;
+
+     @Column()
+     email: string;
+
+     @Column()
+     password: string;
+
+     @Column({
+        type:'enum',
+        enum:Gender,
+        default:Gender.PreferNotToSay,
+        nullable:false,
+     })
+     gender: Gender;
+
+     @Column({
+        type:'enum',
+        enum:UserType,
+        default:UserType.Student,
+        nullable:false,
+        
+     })
+     userType: UserType;
+
+     @Column()
+     title: string;
+
+
+
+}
+
+
+
