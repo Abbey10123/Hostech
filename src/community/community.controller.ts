f9c857f8695644225d098e4f053b8b02efab504d
Abbey10123 <abbeyraheem23@gmail.com>
2023-02-14 11:35:56 +0100

    OTP

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CommunityService } from './community.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { User } from './interface/user.interface';
 
 @Controller('user')
@@ -7,12 +9,12 @@ export class CommunityController {
   constructor(private readonly communityService: CommunityService) {}
 
   @Post()
-  create(@Body() user: User) {
+  create(@Body() user: CreateUserDto) {
     return this.communityService.createUser(user);
   }
 
   @Post('login')
-  login(@Body() loginInfo: User) {
+  login(@Body() loginInfo: LoginUserDto) {
     // and this
     return this.communityService.login(loginInfo);
   }
