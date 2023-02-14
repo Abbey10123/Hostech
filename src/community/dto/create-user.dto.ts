8d6d88adc46d9fd1163de6593d0b53067c215a2c
Abbey10123 <abbeyraheem23@gmail.com>
2023-02-14 12:26:13 +0100

    new otp

---
+++ src/community/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, MinLength } from "class-validator"
+
+export class CreateUserDto {
+    @IsEmail()
+    email:string;
+
+    @MinLength(5)
+    fullName: string;
+
+    @MinLength(8)
+    password: string;
+
+}
---
+++ src/community/entities/otp.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Otp, OtpReason } from "../interface/otp.interface";
+
+@Entity({name: "otps"})
+
+export class OtpEntity implements Otp{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({type:"int", name:"user_id"})
+    userId: number;
+
+    @Column()
+    code: string;
+
+    @Column({type: "timestamp"})
+    expiryDate: Date;
+
+    @Column({type:"enum", enum: OtpReason, default:OtpReason.verifyEmail, nullable:false})
+    reason: OtpReason;
+}
