ea499f9ca99378458d77c7750b0b2497b890f504
Abbey10123 <118048899+Abbey10123@users.noreply.github.com>
2023-02-14 12:32:25 +0100

    Revert "Feature/newotp"

--- src/community/helpers/otp.helper.ts
+++
@@ -1,6 +0,0 @@
-export function generateOtp(){ 
-    const min = 100000;
-    const max = 999999;
-    return Math.ceil(Math.random() * (max - min) + min)
-    
-}
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TransactionEntity } from './entities/transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransactionEntity])],
   providers: [TransactionsService],
   controllers: [TransactionsController]
 })
